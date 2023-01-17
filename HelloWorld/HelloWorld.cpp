a8a3a47d874508adc6929b348044a1b337f657e5
Ludwig Füchsl <ludwig@fuechsl.org>
2023-01-17 18:32:42 +0100

    Added initial files

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,11 @@
+﻿// HelloWorld.cpp : Defines the entry point for the application.
+//
+
+#include "HelloWorld.h"
+
+#include <typeinfo>
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+}
